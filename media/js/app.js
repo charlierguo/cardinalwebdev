7432f0636bfd9f619102b91abe121eb61644e408
Kevin Xu <kevin@imkevinxu.com>
2013-01-07 15:03:50 -0800

    updated app.js

--- media/js/app.js
+++ media/js/app.js
@@ -1,10 +1,62 @@
 ;(function ($, window, undefined) {
 
-  /* Javascript masterminded by Kevin Xu <kevin@imkevinxu.com> */
+  /* -----------------------------------------
+     EMAIL FORM
+  ----------------------------------------- */
+  function emailSubmit() {
+    var form = $(this);
+    if ($(this).hasClass('submit_button')) {
+      form = $(this).parents('form');
+    }
 
+    var input = form.find('input[type=email]');
+    form.find('.thanks').fadeOut().remove();
+    if (input.val()) {
+      $.ajax({
+        type: 'POST',
+        url: '/email/',
+        data: { email: input.val() },
+        success: function(data) {
+          if (data['status'] === 'success') {
+            input.prop('disabled', true).removeClass('error');
+            form.find('.submit_button').removeClass('error').addClass('success animated roll');
+            window.setTimeout( function() {
+              thanks = $('<p>', {
+                class: 'thanks',
+                html: data['msg'],
+              });
+              thanks.hide().appendTo(form.find('.email_submit')).fadeIn();
+              form.find('.submit_button').remove();
+            }, 1000);
+          } else {
+            input.addClass('error');
+            form.find('.submit_button').addClass('error');
+            thanks = $('<p>', {
+              class: 'thanks error',
+              style: 'margin-left: 10px',
+              html: data['msg'],
+            });
+            thanks.hide().appendTo(form.find('.email_submit')).fadeIn();
+          }
+        }
+      });
+    } else {
+      input.addClass('animated shake');
+      window.setTimeout( function() {
+        input.removeClass('animated shake').focus()},
+        1000
+      );
+    }
 
+    return false;
+  }
 
+  $('.contactus form').submit(emailSubmit);
+  $('.submit_button').click(emailSubmit);
 
+  $('.interested').click(function() {
+    $(this).parent().next().find('input[type=email]').focus();
+  });
 
   /* ZURB Foundation Javascript Initialization */
   var $doc = $(document);
@@ -25,7 +77,7 @@
     $.fn.placeholder                ? $('input, textarea').placeholder() : null;
   });
 
-  $("#featured").orbit();
+  $('#featured').orbit();
   $('input, textarea').placeholder();
 
   // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
@@ -42,5 +94,39 @@
       }, 0);
     });
   }
+
+  /* -----------------------------------------
+     AUTO-SETS CSRF TOKEN FOR AJAX CALLS
+  ----------------------------------------- */
+
+  // Acquiring CSRF token and setting it to X-CSRFToken header for AJAX POST Request
+  function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = jQuery.trim(cookies[i]);
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
+  var csrftoken = getCookie('csrftoken');
+  function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  }
+  $.ajaxSetup({
+    crossDomain: false, // obviates need for sameOrigin test
+    beforeSend: function(xhr, settings) {
+      if (!csrfSafeMethod(settings.type)) {
+        xhr.setRequestHeader('X-CSRFToken', csrftoken);
+      }
+    }
+  });
 
 })(jQuery, this);
